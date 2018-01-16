dd1fdb4209795c6114156bfb6d48bb32dcd7d87d
Sindhusha <srisindhusha10@gmail.com>
2018-01-16 11:40:36 -0500

    First commit

---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from '../shared/shopping-cart.service';
+import { Router } from '@angular/router';
+
+@Component({
+    templateUrl: './cart.component.html',
+    styleUrls: ['./cart.component.css']
+})
+
+export class CartComponent implements OnInit {
+    myCart: any[];
+    cartTotal: number;
+    constructor( private cartSVC: ShoppingCartService, private router: Router) {}
+
+    ngOnInit(){
+        this.cartSVC.getCart()
+            .then(theCart => this.myCart = theCart)
+            .then(cart => this.sumCart(cart))
+            .then(sum => this.cartTotal = sum);
+    }
+
+    sumCart(cart: any) {
+        return Promise.resolve(cart.reduce((total: number, item: any) => total + item.price, 0));
+    }
+
+    removeCart(id: string) {
+        this.cartSVC.removeCart(id);
+        this.sumCart(this.myCart).then(sum => this.cartTotal = sum);
+    }
+
+    purchase(){
+        alert(`Your order totaled ${this.cartTotal}`);
+        this.router.navigate(['/shop']);
+    }
+
+    cancel(){
+        this.router.navigate(['/shop']);
+    }
+}
